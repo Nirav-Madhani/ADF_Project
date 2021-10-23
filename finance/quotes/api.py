c839195b51fdf7774e151cfcf2da23a5d2808d1d
Nirav <18bce135@nirmauni.ac.in>
2021-10-23 09:23:59 +0530

    Update api.py

--- finance/quotes/api.py
+++ finance/quotes/api.py
@@ -12,10 +12,11 @@ def GetAllStocks(tickerString):
 def get_symbol_list(symbol):
     url = "https://query1.finance.yahoo.com/v1/finance/search?q={}".format(symbol)
 
-    result = requests.get(url).json()
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    result = requests.get(url, headers=headers)
+    result = result.content.decode()
+    print(result)
+    return result
 
-    for x in result['ResultSet']['Result']:
-        if x['symbol'] == symbol:
-            return x['name']
 
-
+get_symbol_list("Micro")
